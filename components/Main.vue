ae00fa7d60eea78ace68b3f7118ab8db45a9b5ae
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-05-30 10:23:40 +0900

    add: can move card

--- components/Main.vue
+++ components/Main.vue
@@ -3,17 +3,26 @@
     <panel class="main__panel">
       <template slot="header">欲しい</template>
       <div slot="body">
-        <card v-for="item in want" :key="item.id" :item="item"></card>
+        <draggable v-model="want" group="items">
+          <card v-for="item in want" :key="item.id" :item="item"></card>
+        </draggable>
       </div>
     </panel>
     <panel class="main__panel">
       <template slot="header">買う</template>
       <div slot="body">
-        <card v-for="item in will" :key="item.id" :item="item"></card>
+        <draggable v-model="will" group="items">
+          <card v-for="item in will" :key="item.id" :item="item"></card>
+        </draggable>
       </div>
     </panel>
     <panel class="main__panel">
       <template slot="header">買った</template>
+      <div slot="body">
+        <draggable v-model="done" group="items">
+          <card v-for="item in done" :key="item.id" :item="item"></card>
+        </draggable>
+      </div>
     </panel>
     <panel class="main__panel">
       <template slot="header">統計</template>
@@ -22,11 +31,14 @@
 </template>
 
 <script>
+import Draggable from "vuedraggable";
+
 import Panel from "./Panel";
 import Card from "./Card";
 
 export default {
   components: {
+    Draggable,
     Panel,
     Card
   },
